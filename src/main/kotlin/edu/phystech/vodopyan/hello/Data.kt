00d5086caf7ff44eeb2d829463dca2a288ba7c62
voddan <dgvodopyan@gmail.com>
2017-05-24 10:58:07 +0300

    added info on supervisors by Igor Molibog

--- src/main/kotlin/edu/phystech/vodopyan/hello/Data.kt
+++ src/main/kotlin/edu/phystech/vodopyan/hello/Data.kt
@@ -53,14 +53,24 @@ data class Supervisor(
 
 
 val organisations: Map<String, Organisation> = listOf(
-        Organisation("ВЦ РАН", URL("http://wikimipt.org/wiki/%D0%92%D1%8B%D1%87%D0%B8%D1%81%D0%BB%D0%B8%D1%82%D0%B5%D0%BB%D1%8C%D0%BD%D1%8B%D0%B9_%D0%A6%D0%B5%D0%BD%D1%82%D1%80_%D0%B8%D0%BC._%D0%90._%D0%90._%D0%94%D0%BE%D1%80%D0%BE%D0%B4%D0%BD%D0%B8%D1%86%D1%8B%D0%BD%D0%B0_%D0%A0%D0%90%D0%9D"))
+        Organisation("ВЦ РАН", URL("http://wikimipt.org/wiki/%D0%92%D1%8B%D1%87%D0%B8%D1%81%D0%BB%D0%B8%D1%82%D0%B5%D0%BB%D1%8C%D0%BD%D1%8B%D0%B9_%D0%A6%D0%B5%D0%BD%D1%82%D1%80_%D0%B8%D0%BC._%D0%90._%D0%90._%D0%94%D0%BE%D1%80%D0%BE%D0%B4%D0%BD%D0%B8%D1%86%D1%8B%D0%BD%D0%B0_%D0%A0%D0%90%D0%9D")),
+        Organisation("ИППИ", URL("http://iitp.ru/ru/about")),
+        Organisation("Parallels", URL("https://mipt.ru/education/chairs/theor_app_informatics/"))
 ).associateBy { it.name }
 
 
 val laboratories: Map<String, Laboratory> = listOf(
         Laboratory("Интеллектуальные системы",
                 listOf(Subject.math, Subject.info), listOf(Department.fupm), listOf(organisations["ВЦ РАН"]!!),
-                URL("http://wikimipt.org/wiki/%D0%9A%D0%B0%D1%84%D0%B5%D0%B4%D1%80%D0%B0_%C2%AB%D0%98%D0%BD%D1%82%D0%B5%D0%BB%D0%BB%D0%B5%D0%BA%D1%82%D1%83%D0%B0%D0%BB%D1%8C%D0%BD%D1%8B%D0%B5_%D1%81%D0%B8%D1%81%D1%82%D0%B5%D0%BC%D1%8B%C2%BB"))
+                URL("http://wikimipt.org/wiki/%D0%9A%D0%B0%D1%84%D0%B5%D0%B4%D1%80%D0%B0_%C2%AB%D0%98%D0%BD%D1%82%D0%B5%D0%BB%D0%BB%D0%B5%D0%BA%D1%82%D1%83%D0%B0%D0%BB%D1%8C%D0%BD%D1%8B%D0%B5_%D1%81%D0%B8%D1%81%D1%82%D0%B5%D0%BC%D1%8B%C2%BB")),
+
+        Laboratory("Кафедра проблем передачи информации и анализа данных",
+                listOf(Subject.math, Subject.info), listOf(Department.fupm), listOf(organisations["ИППИ"]!!),
+                URL("http://iitp.ru/ru/about/chair.mipt")),
+
+        Laboratory("Теоретическая и прикладная информатика",
+                listOf(Subject.math, Subject.info), listOf(Department.fupm), listOf(organisations["Parallels"]!!),
+                URL("https://mipt.ru/dcam/basechairs/parallels.php"))
 ).associateBy { it.name }
 
 
@@ -68,9 +78,33 @@ val supervisors: Map<String, Supervisor> = listOf(
         Supervisor("Воронцов Константин Вячеславович",
                 listOf(laboratories["Интеллектуальные системы"]!!),
                 URL("http://wikimipt.org/wiki/%D0%92%D0%BE%D1%80%D0%BE%D0%BD%D1%86%D0%BE%D0%B2_%D0%9A%D0%BE%D0%BD%D1%81%D1%82%D0%B0%D0%BD%D1%82%D0%B8%D0%BD_%D0%92%D1%8F%D1%87%D0%B5%D1%81%D0%BB%D0%B0%D0%B2%D0%BE%D0%B2%D0%B8%D1%87"),
-                URL("http://wikimipt.org/images/thumb/5/56/%D0%92%D0%BE%D1%80%D0%BE%D0%BD%D1%86%D0%BE%D0%B2_%D0%9A%D0%BE%D0%BD%D1%81%D1%82%D0%B0%D0%BD%D1%82%D0%B8%D0%BD_%D0%92%D1%8F%D1%87%D0%B5%D1%81%D0%BB%D0%B0%D0%B2%D0%BE%D0%B2%D0%B8%D1%87.jpeg/266px-%D0%92%D0%BE%D1%80%D0%BE%D0%BD%D1%86%D0%BE%D0%B2_%D0%9A%D0%BE%D0%BD%D1%81%D1%82%D0%B0%D0%BD%D1%82%D0%B8%D0%BD_%D0%92%D1%8F%D1%87%D0%B5%D1%81%D0%BB%D0%B0%D0%B2%D0%BE%D0%B2%D0%B8%D1%87.jpeg"))
-).associateBy { it.name }
+                URL("http://wikimipt.org/images/thumb/5/56/%D0%92%D0%BE%D1%80%D0%BE%D0%BD%D1%86%D0%BE%D0%B2_%D0%9A%D0%BE%D0%BD%D1%81%D1%82%D0%B0%D0%BD%D1%82%D0%B8%D0%BD_%D0%92%D1%8F%D1%87%D0%B5%D1%81%D0%BB%D0%B0%D0%B2%D0%BE%D0%B2%D0%B8%D1%87.jpeg/266px-%D0%92%D0%BE%D1%80%D0%BE%D0%BD%D1%86%D0%BE%D0%B2_%D0%9A%D0%BE%D0%BD%D1%81%D1%82%D0%B0%D0%BD%D1%82%D0%B8%D0%BD_%D0%92%D1%8F%D1%87%D0%B5%D1%81%D0%BB%D0%B0%D0%B2%D0%BE%D0%B2%D0%B8%D1%87.jpeg")),
 
+        Supervisor("Спокойный Владимир Григорьевич",
+                listOf(laboratories["Кафедра проблем передачи информации и анализа данных"]!!),
+                URL("http://faculty.skoltech.ru/people/vladimirspokoiny"),
+                URL("http://www.skoltech.ru/app/data/uploads/sites/19/2016/07/vladimirspokoiny_avatar_1467806260.jpg")),
+
+        Supervisor("Чехович Юрий Викторович",
+                listOf(laboratories["Интеллектуальные системы"]!!),
+                URL("http://www.machinelearning.ru/wiki/index.php?title=%D0%A3%D1%87%D0%B0%D1%81%D1%82%D0%BD%D0%B8%D0%BA:Yury_Chekhovich"),
+                URL("http://www.machinelearning.ru/wiki/images/thumb/8/80/Chehovich.JPG/484px-Chehovich.JPG")),
+
+        Supervisor("Лунев Денис Владимирович",
+                listOf(laboratories["Теоретическая и прикладная информатика"]!!),
+                URL("http://wikimipt.org/wiki/%D0%9B%D1%83%D0%BD%D1%91%D0%B2_%D0%94%D0%B5%D0%BD%D0%B8%D1%81_%D0%92%D0%BB%D0%B0%D0%B4%D0%B8%D0%BC%D0%B8%D1%80%D0%BE%D0%B2%D0%B8%D1%87"),
+                URL("http://wikimipt.org/images/0/08/%D0%9B%D1%83%D0%BD%D1%91%D0%B2_%D0%94%D0%B5%D0%BD%D0%B8%D1%81_%D0%92%D0%BB%D0%B0%D0%B4%D0%B8%D0%BC%D0%B8%D1%80%D0%BE%D0%B2%D0%B8%D1%87.jpeg")),
+
+        Supervisor("Вадим Викторович Стрижов",
+                listOf(laboratories["Интеллектуальные системы"]!!),
+                URL("http://www.machinelearning.ru/wiki/index.php?title=%D0%A3%D1%87%D0%B0%D1%81%D1%82%D0%BD%D0%B8%D0%BA:Strijov"),
+                URL("http://www.machinelearning.ru/wiki/images/c/c6/Strijov.jpg")),
+
+        Supervisor("Максимов Юрий Владимирович",
+                listOf(laboratories["Кафедра проблем передачи информации и анализа данных"]!!),
+                URL("http://faculty.skoltech.ru/people/yurymaximov"),
+                URL("http://www.skoltech.ru/app/data/uploads/sites/19/2016/07/yurymaximov_avatar_1467559189.jpg"))
+).associateBy { it.name }
 
 
 
